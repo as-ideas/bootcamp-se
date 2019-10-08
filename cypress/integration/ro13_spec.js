0a10769783f499af5e2006064694a1daa1e77c45
Julian Godea <julian.godesa@asideas.de>
2019-10-08 10:53:12 +0200

    Revert "removes at for rot13"
    This reverts commit 174f357bcbc72a9121ff7a6ea38ccb80d9b260ae.

---
+++ cypress/integration/ro13_spec.js
@@ -0,0 +1,26 @@
+describe('ROT13 Website', function () {
+    it('allows to encode text using rot13', function () {
+        cy.visit('')
+
+
+        var input = "abc 123 xyz"
+        var expected = "nop 123 klm"
+        cy.get('#input_encode').type(input)
+
+        cy.get('#encode').click()
+
+        cy.get('#input_decode').should('equal', expected)
+    })
+
+    it('allows to decode text using rot13', function () {
+        cy.visit('')
+
+        var input = "nop 123 klm"
+        var expected = "abc 123 xyz"
+        cy.get('#input_decode').type(input)
+
+        cy.get('#decode').click()
+
+        cy.get('#input_encode').should('equal', expected)
+    })
+})
